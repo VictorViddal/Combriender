d387c40319e61deafde50270c4ef3e458666e8da
vidal <victor.matheus.o.vidal@gmail.com>
2021-11-08 14:09:17 -0400

    Create model for TableViewCell

---
+++ Combriender/Combine/Cells/PokemonTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PokemonTableViewCell.swift
+//  Combriender
+//
+//  Created by Victor Vidal on 08/11/21.
+//
+
+import UIKit
+
+class PokemonTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
