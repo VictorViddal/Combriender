20b95a4ec546bf6f76c1189d68b3ce429652927b
vidal <victor.matheus.o.vidal@gmail.com>
2021-11-08 14:02:25 -0400

    Creating model for API

---
+++ Combriender/Combine/Model/PokemonModel.swift
@@ -0,0 +1,27 @@
+//
+//  PokemonModel.swift
+//  Combriender
+//
+//  Created by Victor Vidal on 08/11/21.
+//
+
+import Foundation
+
+struct Pokemon: Codable {
+    var name: String?
+    var url: String?
+}
+
+struct PokemonData: Codable {
+    var abilities: [Ability?]
+    var height: Int?
+    var weight: Int?
+    var id: Int?
+    var is_default: Bool?
+    
+    struct Ability: Codable {
+        var name: String?
+        var is_hidden: Bool?
+        var slot: Int?
+    }
+}
