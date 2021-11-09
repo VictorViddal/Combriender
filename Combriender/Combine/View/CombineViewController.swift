21b8f4daf4a2a232bf152b9a86f3134de44e44fb
vidal <victor.matheus.o.vidal@gmail.com>
2021-11-09 14:41:42 -0400

    Create navigation for Combine and setting for the other features

--- Combriender/Combine/View/CombineViewController.swift
+++ Combriender/Combine/View/CombineViewController.swift
@@ -12,11 +12,11 @@ class CombineViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        settingTableViewCell()
         // Do any additional setup after loading the view.
     }
 
-    func settingTableViewCell() {
+    private func settingTableViewCell() {
         tableView.register(UINib(nibName: "PokemonTableViewCell", bundle: .main), forCellReuseIdentifier: "PokemonTableViewCell")
     }
 }
@@ -28,7 +28,7 @@ extension CombineViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "PokemonTableViewCell.", for: indexPath) as? PokemonTableViewCell else {return UITableViewCell()}
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "PokemonTableViewCell", for: indexPath) as? PokemonTableViewCell else {return UITableViewCell()}
         cell.pokemonImage.image = UIImage(systemName: "heart")
         cell.pokemonName.text = "teste"
         return cell
