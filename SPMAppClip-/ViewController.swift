585a6c57cb30a934a421dd1af2d6198d79d4fe07
Vinícius Pinheiro <viniciuspinheiro@MacBook-Pro-de-Vinicius.local>
2021-11-24 14:35:47 -0400

    Adicionado o App Clip do SPM

---
+++ SPMAppClip-/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SPMAppClip-
+//
+//  Created by Vinícius Pinheiro on 24/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
